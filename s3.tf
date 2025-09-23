12fffceaa612e1699ad12e30a5634d23cfe1f633
cvalencia86 <cristian.valencia@e360.com>
2025-09-23 15:48:43 -0400

    added a billing code tag

--- s3.tf
+++ s3.tf
@@ -8,7 +8,10 @@ resource "aws_s3_object" "terraform-github-object" {
   bucket  = aws_s3_bucket.terraform-github-bucket.id
   key     = "multiplebranches.txt"
   content = "this is a sample file to test multiple branches in github with terraform cloud"
-  tags = {
-    BillingCode = var.aws_tags
-  }
+}
+
+resource "aws_tag" "s3-bucket-tag" {
+  resource_id = aws_s3_bucket.terraform-github-bucket.id
+  key         = "BillingCode"
+  value       = var.aws_tags
 }
